4c494afec80eaa0cce3c982af72c03758fea481b
faram <romanchykov.webdev@gmail.com>
2024-10-12 11:31:25 +0200

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WelcomeScreen = () => {
+  return (
+    <View >
+      <Text>WelcomeScreen works!</Text>
+    </View>
+  );
+};
+
+
+export default WelcomeScreen;
